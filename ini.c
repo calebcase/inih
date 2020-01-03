a77bef0f7cd58983ef858e39516ba47ddf29363f
ccase <caleb@storj.io>
2020-01-03 06:38:17 -0500

    Fix gcc warnings about strncpy.

--- ini.c
+++ ini.c
@@ -74,8 +74,9 @@ static char* find_chars_or_comment(const char* s, const char* chars)
 /* Version of strncpy that ensures dest (size bytes) is null-terminated. */
 static char* strncpy0(char* dest, const char* src, size_t size)
 {
-    strncpy(dest, src, size - 1);
-    dest[size - 1] = '\0';
+    size_t n = strnlen(src, size - 1);
+    memcpy(dest, src, n);
+    dest[n] = '\0';
     return dest;
 }
 
